330ac203bebcf904b04adf6a37dddbc4f950ef70
Charles Hoffman <chuck.hoffman@banno.com>
2017-05-05 11:51:39 -0500

    hell, this old crap don't even compile nm

--- ex5.scala
+++ ex5.scala
@@ -1,167 +1,167 @@
-object ex5 {
+// object Ex5 {
 
-  sealed trait Stream[+A] {
-    def headOption: Option[A] = this match {
-      case Empty => None
-      case Cons(h, t) => Some(h())
-    }
+//   sealed trait Stream[+A] {
+//     def headOption: Option[A] = this match {
+//       case Empty => None
+//       case Cons(h, t) => Some(h())
+//     }
 
-    // 5.1
-    def toList: List[A] = this match {
-      case Empty => List()
-      case Cons(h, t) => h() +: t().toList
-    }
+//     // 5.1
+//     def toList: List[A] = this match {
+//       case Empty => List()
+//       case Cons(h, t) => h() +: t().toList
+//     }
 
-    // 5.2
-    def take(n: Int): Stream[A] = (this, n) match {
-      case (_, 0)          => Empty
-      case (Empty, _)      => Empty //error("ran out of stream!")
-      case (Cons(h, t), _) => Cons(h, () => t().take(n-1))
-    }
+//     // 5.2
+//     def take(n: Int): Stream[A] = (this, n) match {
+//       case (_, 0)          => Empty
+//       case (Empty, _)      => Empty //error("ran out of stream!")
+//       case (Cons(h, t), _) => Cons(h, () => t().take(n-1))
+//     }
 
-    def drop(n: Int): Stream[A] = (this, n) match {
-      case (_, 0)          => this
-      case (Empty, _)      => this //error("ran out of stream!")
-      case (Cons(h, t), _) => t().drop(n-1)
-    }
+//     def drop(n: Int): Stream[A] = (this, n) match {
+//       case (_, 0)          => this
+//       case (Empty, _)      => this //error("ran out of stream!")
+//       case (Cons(h, t), _) => t().drop(n-1)
+//     }
 
-    // 5.3
-    def takeWhile0(p: A => Boolean): Stream[A] = this match {
-      case Cons(h, t) if (p(h())) => Cons(h, () => t().takeWhile0(p))
-      case _ => Empty
-    }
+//     // 5.3
+//     def takeWhile0(p: A => Boolean): Stream[A] = this match {
+//       case Cons(h, t) if (p(h())) => Cons(h, () => t().takeWhile0(p))
+//       case _ => Empty
+//     }
 
-    def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f))
-      case _ => z
-    }
+//     def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+//       case Cons(h,t) => f(h(), t().foldRight(z)(f))
+//       case _ => z
+//     }
 
-    def exists(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
+//     def exists(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
 
-    // 5.4
-    def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
+//     // 5.4
+//     def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
 
-    // 5.5
-    // works but is weird/long
-//    import Stream._
-    def takeWhile(p: A => Boolean): Stream[A] =
-      foldRight(Empty: Stream[A])((a, b) =>
-        if (p(a)) cons(a, b)
-        else Empty)
+//     // 5.5
+//     // works but is weird/long
+// //    import Stream._
+//     def takeWhile(p: A => Boolean): Stream[A] =
+//       foldRight(Empty: Stream[A])((a, b) =>
+//         if (p(a)) Cons(a, b)
+//         else Empty)
 
-    // 5.6
-    def headOption0: Option[A] = foldRight(None: Option[A])((a, b) => Some(a))
+//     // 5.6
+//     def headOption0: Option[A] = foldRight(None: Option[A])((a, b) => Some(a))
 
-    // 5.7
-    // def map[A, B](f: A => B) =
-    //   foldRight(Empty: Stream[B])((h, t) => Cons(() => f(h), () => t))
-    def map[B](f: A => B): Stream[B] =
-      foldRight(empty[B])((h,t) => cons(f(h), t))
+//     // 5.7
+//     // def map[A, B](f: A => B) =
+//     //   foldRight(Empty: Stream[B])((h, t) => Cons(() => f(h), () => t))
+//     def map[B](f: A => B): Stream[B] =
+//       foldRight(Empty[B])((h,t) => cons(f(h), t))
 
-    def append[B>:A](s: => Stream[B]): Stream[B] =
-      foldRight(s)((h, t) => cons(h, t))
+//     def append[B>:A](s: => Stream[B]): Stream[B] =
+//       foldRight(s)((h, t) => Cons(h, t))
 
-    def flatMap[B](f: A => Stream[B]): Stream[B] =
-      foldRight(empty[B])((h, t) => f(h).append(t))
+//     def flatMap[B](f: A => Stream[B]): Stream[B] =
+//       foldRight(Empty[B])((h, t) => f(h).append(t))
 
-    // why doesVthis have to be a B? O_o
-    def filter[B](f: A => Boolean): Stream[A] =
-      foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else t)
+//     // why doesVthis have to be a B? O_o
+//     def filter[B](f: A => Boolean): Stream[A] =
+//       foldRight(Empty[A])((h, t) => if (f(h)) cons(h, t) else t)
 
-    // 5.13
-    def umap[B](f: A => B): Stream[B] = unfold(this){
-      case Cons(h, t) => Some((f(h()), t()))
-      case _          => None
-    }
+//     // 5.13
+//     def umap[B](f: A => B): Stream[B] = unfold(this){
+//       case Cons(h, t) => Some((f(h()), t()))
+//       case _          => None
+//     }
 
-    def utake(n: Int): Stream[A] = unfold((this, n)){
-      case (_, 0) | (Empty, _) => None
-      case (Cons(h, t), n)     => Some((h(), (t(), n-1)))
-    }
+//     def utake(n: Int): Stream[A] = unfold((this, n)){
+//       case (_, 0) | (Empty, _) => None
+//       case (Cons(h, t), n)     => Some((h(), (t(), n-1)))
+//     }
 
-    def utakeWhile(p: A => Boolean): Stream[A] = unfold(this){
-      case Empty      => None
-      case Cons(h, t) => if (p(h())) Some((h(), t())) else None
-    }
+//     def utakeWhile(p: A => Boolean): Stream[A] = unfold(this){
+//       case Empty      => None
+//       case Cons(h, t) => if (p(h())) Some((h(), t())) else None
+//     }
 
-    // i misunderstood the meaning of "with" here (didn't refer back to ch. 3)
-    def uzipWith[B](those: Stream[B]): Stream[(A, B)] = unfold((this, those)){
-      case (Empty, _) | (_, Empty) => None
-      case (Cons(ah, at), Cons(bh, bt)) => Some((ah(), bh()), (at(), bt()))
-    }
+//     // i misunderstood the meaning of "with" here (didn't refer back to ch. 3)
+//     def uzipWith[B](those: Stream[B]): Stream[(A, B)] = unfold((this, those)){
+//       case (Empty, _) | (_, Empty) => None
+//       case (Cons(ah, at), Cons(bh, bt)) => Some((ah(), bh()), (at(), bt()))
+//     }
 
-    def uzipAll[B](those: Stream[B]): Stream[(Option[A], Option[B])] = unfold((this, those)){
-      case (Empty, Empty)               => None
-      case (Empty, Cons(bh, bt))        => Some((None,       Some(bh())), (Empty, bt()))
-      case (Cons(ah, at), Empty)        => Some((Some(ah()), None),       (at(), Empty))
-      case (Cons(ah, at), Cons(bh, bt)) => Some((Some(ah()), Some(bh())), (at(), bt()))
-    }
+//     def uzipAll[B](those: Stream[B]): Stream[(Option[A], Option[B])] = unfold((this, those)){
+//       case (Empty, Empty)               => None
+//       case (Empty, Cons(bh, bt))        => Some((None,       Some(bh())), (Empty, bt()))
+//       case (Cons(ah, at), Empty)        => Some((Some(ah()), None),       (at(), Empty))
+//       case (Cons(ah, at), Cons(bh, bt)) => Some((Some(ah()), Some(bh())), (at(), bt()))
+//     }
 
-    // 5.14
-    def startsWith[A](what: Stream[A]): Boolean = (this, what) match {
-      case (_, Empty) => true
-      case (Empty, _) => false
-      case (Cons(h1, t1), Cons(h2, t2)) => (h1() == h2()) && t1().startsWith(t2())
-    }
+//     // 5.14
+//     def startsWith[A](what: Stream[A]): Boolean = (this, what) match {
+//       case (_, Empty) => true
+//       case (Empty, _) => false
+//       case (Cons(h1, t1), Cons(h2, t2)) => (h1() == h2()) && t1().startsWith(t2())
+//     }
 
-    // 5.15
-    def tails: Stream[Stream[A]] = unfold(this){
-      case Empty        => None
-      case (Cons(_, t)) => Some((t(), t()))
-    }
+//     // 5.15
+//     def tails: Stream[Stream[A]] = unfold(this){
+//       case Empty        => None
+//       case (Cons(_, t)) => Some((t(), t()))
+//     }
 
-    def hasSubsequence[A](s: Stream[A]): Boolean = tails exists (_ startsWith s)
+//     def hasSubsequence[A](s: Stream[A]): Boolean = tails exists (_ startsWith s)
 
-    // 5.16
-    // did anyone figure this out? I don't think this solution is linear time
-    // as required, even if it did compile
-    // def scanRight(z: => A)(f: (A, => A) => A): Stream[A] = unfold(this){
-    //   case Empty        => None
-    //   case (Cons(_, t)) => Some((t().foldRight(z)(f), t()))
-    // }
-  }
+//     // 5.16
+//     // did anyone figure this out? I don't think this solution is linear time
+//     // as required, even if it did compile
+//     // def scanRight(z: => A)(f: (A, => A) => A): Stream[A] = unfold(this){
+//     //   case Empty        => None
+//     //   case (Cons(_, t)) => Some((t().foldRight(z)(f), t()))
+//     // }
+//   }
 
-  case object Empty extends Stream[Nothing]
-  case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+//   case object Empty extends Stream[Nothing]
+//   case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
-  object Stream {
-    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
-      lazy val head = hd
-      lazy val tail = tl
-      Cons(() => head, () => tail)
-    }
+//   object Stream {
+//     def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+//       lazy val head = hd
+//       lazy val tail = tl
+//       Cons(() => head, () => tail)
+//     }
 
-    def empty[A]: Stream[A] = Empty
+//     def empty[A]: Stream[A] = Empty
 
-    def apply[A](as: A*): Stream[A] =
-      if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+//     def apply[A](as: A*): Stream[A] =
+//       if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
-    // 5.8
-    def constant[A](a: A): Stream[A] = cons(a, constant(a))
+//     // 5.8
+//     def constant[A](a: A): Stream[A] = cons(a, constant(a))
 
-    // 5.9
-    def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+//     // 5.9
+//     def from(n: Int): Stream[Int] = cons(n, from(n + 1))
 
-    // 5.10
-    def fibs: Stream[Int] = {
-      def fibsFrom(n1: Int, n2: Int): Stream[Int] =
-        cons(n1, fibsFrom(n2, n1+n2))
-      fibsFrom(0, 1)
-    }
+//     // 5.10
+//     def fibs: Stream[Int] = {
+//       def fibsFrom(n1: Int, n2: Int): Stream[Int] =
+//         cons(n1, fibsFrom(n2, n1+n2))
+//       fibsFrom(0, 1)
+//     }
 
-    // 5.11
-    def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
-      case None => empty
-      case Some((a, s)) => cons(a, unfold(s)(f))
-    }
+//     // 5.11
+//     def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+//       case None => empty
+//       case Some((a, s)) => cons(a, unfold(s)(f))
+//     }
 
-    // 5.12
-    def ufibs: Stream[Int] = Stream.unfold((0, 1))(s => Some(s._1, (s._2, s._1+s._2)))
+//     // 5.12
+//     def ufibs: Stream[Int] = Stream.unfold((0, 1))(s => Some(s._1, (s._2, s._1+s._2)))
 
-    def uconstant[A](a: A): Stream[A] = unfold(a)(s => Some((s, s)))
+//     def uconstant[A](a: A): Stream[A] = unfold(a)(s => Some((s, s)))
 
-    def ufrom(n: Int): Stream[Int] = unfold(n)(s => Some((s, s+1)))
+//     def ufrom(n: Int): Stream[Int] = unfold(n)(s => Some((s, s+1)))
 
-    def uones = uconstant(1)
-  }
-}
+//     def uones = uconstant(1)
+//   }
+// }
--- ex6.scala
+++ ex6.scala
@@ -1,178 +1,178 @@
-object ex6 {
+// object ex6 {
 
-  trait RNG {
-    def nextInt: (Int, RNG)
-  }
+//   trait RNG {
+//     def nextInt: (Int, RNG)
+//   }
 
-  case class SimpleRNG(seed: Long) extends RNG {
-    def nextInt: (Int, RNG) = {
-      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-      val nextRNG = SimpleRNG(newSeed)
-      val n = (newSeed >>> 16).toInt
-      (n, nextRNG)
-    }
-  }
+//   case class SimpleRNG(seed: Long) extends RNG {
+//     def nextInt: (Int, RNG) = {
+//       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+//       val nextRNG = SimpleRNG(newSeed)
+//       val n = (newSeed >>> 16).toInt
+//       (n, nextRNG)
+//     }
+//   }
 
-  // 6.1
-  def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (nn, nextRNG) = rng.nextInt
-    val n = if (nn < 0) -(nn + 1) else nn
-    //val n = if (nn < -Int.MaxValue) -nn else Int.MaxValue
-    (n, nextRNG)
-  }
+//   // 6.1
+//   def nonNegativeInt(rng: RNG): (Int, RNG) = {
+//     val (nn, nextRNG) = rng.nextInt
+//     val n = if (nn < 0) -(nn + 1) else nn
+//     //val n = if (nn < -Int.MaxValue) -nn else Int.MaxValue
+//     (n, nextRNG)
+//   }
 
-  // 6.2
-  def double(rng: RNG): (Double, RNG) = {
-    val (nn, nextRNG) = nonNegativeInt(rng)
-    val d =  nn / (Int.MaxValue.toDouble+1)
-    (d, nextRNG)
-  }
+//   // 6.2
+//   def double(rng: RNG): (Double, RNG) = {
+//     val (nn, nextRNG) = nonNegativeInt(rng)
+//     val d =  nn / (Int.MaxValue.toDouble+1)
+//     (d, nextRNG)
+//   }
 
-  // def boolean(rng: RNG): (Boolean, RNG) = {
-  //   val (nn, nextRNG) = rng.nextInt
-  //   (nn < 0, nextRNG)
-  // }
+//   // def boolean(rng: RNG): (Boolean, RNG) = {
+//   //   val (nn, nextRNG) = rng.nextInt
+//   //   (nn < 0, nextRNG)
+//   // }
 
-  // 6.3
-  def intDouble(rng: RNG): ((Int,Double), RNG) = {
-    val (n, rng2) = rng.nextInt
-    val (d, rng3) = double(rng2)
-    ((n, d), rng3)
-  }
+//   // 6.3
+//   def intDouble(rng: RNG): ((Int,Double), RNG) = {
+//     val (n, rng2) = rng.nextInt
+//     val (d, rng3) = double(rng2)
+//     ((n, d), rng3)
+//   }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
-    val (d, rng2) = double(rng)
-    val (n, rng3) = rng2.nextInt
-    ((d, n), rng3)
-  }
+//   def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+//     val (d, rng2) = double(rng)
+//     val (n, rng3) = rng2.nextInt
+//     ((d, n), rng3)
+//   }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
-    val (d1, rng2) = double(rng)
-    val (d2, rng3) = double(rng2)
-    val (d3, rng4) = double(rng3)
-    ((d1, d2, d3), rng4)
-  }
+//   def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+//     val (d1, rng2) = double(rng)
+//     val (d2, rng3) = double(rng2)
+//     val (d3, rng4) = double(rng3)
+//     ((d1, d2, d3), rng4)
+//   }
 
-  // 6.4
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    if (count == 0)
-      (List[Int](), rng)
-    else {
-      val (n, nrng)  = rng.nextInt
-      val (ns, frng) = ints(count - 1)(nrng)
-      (n :: ns, frng)
-    }
-  }
+//   // 6.4
+//   def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+//     if (count == 0)
+//       (List[Int](), rng)
+//     else {
+//       val (n, nrng)  = rng.nextInt
+//       val (ns, frng) = ints(count - 1)(nrng)
+//       (n :: ns, frng)
+//     }
+//   }
 
-  type Rand[+A] = RNG => (A, RNG)
+//   type Rand[+A] = RNG => (A, RNG)
 
-  val int: Rand[Int] = _.nextInt
+//   val int: Rand[Int] = _.nextInt
 
-  def unit[A](a: A): Rand[A] = rng => (a, rng)
+//   def unit[A](a: A): Rand[A] = rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
-    val (a, rng2) = s(rng)
-    (f(a), rng2)
-  }
+//   def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+//     val (a, rng2) = s(rng)
+//     (f(a), rng2)
+//   }
 
-  // 6.5
-  def mdouble: Rand[Double] = map(nonNegativeInt)(n => n / (Int.MaxValue.toDouble+1))
+//   // 6.5
+//   def mdouble: Rand[Double] = map(nonNegativeInt)(n => n / (Int.MaxValue.toDouble+1))
 
-  // 6.6
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
-    val (a, rng2) = ra(rng)
-    val (b, rng3) = rb(rng2)
-    (f(a, b), rng3)
-  }
+//   // 6.6
+//   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+//     val (a, rng2) = ra(rng)
+//     val (b, rng3) = rb(rng2)
+//     (f(a, b), rng3)
+//   }
 
-  // 6.7
-  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_,_))
+//   // 6.7
+//   def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_,_))
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
-    fs.foldRight(unit(List[A]()))((hr, tr) => map2(hr, tr)(_::_))
+//   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+//     fs.foldRight(unit(List[A]()))((hr, tr) => map2(hr, tr)(_::_))
 
-  def _ints(count: Int): Rand[List[Int]] = {
-    sequence(List.fill(count)(int))
-  }
+//   def _ints(count: Int): Rand[List[Int]] = {
+//     sequence(List.fill(count)(int))
+//   }
 
-  // "But there are some functions that we can’t very well write in
-  //   terms of map and map2. One such function is
-  //   nonNegativeLessThan, which generates an integer between 0
-  //   (inclusive) and n (exclusive):"
-  // Says who?:
-  def _nonNegativeLessThan(n: Int): Rand[Int] = map(mdouble)(d => (d * n).floor.toInt)
+//   // "But there are some functions that we can’t very well write in
+//   //   terms of map and map2. One such function is
+//   //   nonNegativeLessThan, which generates an integer between 0
+//   //   (inclusive) and n (exclusive):"
+//   // Says who?:
+//   def _nonNegativeLessThan(n: Int): Rand[Int] = map(mdouble)(d => (d * n).floor.toInt)
 
-  def nonNegativeLessThan(n: Int): Rand[Int] = { rng =>
-    val (i, rng2) = nonNegativeInt(rng)
-    val mod = i % n
-    if (i + (n-1) - mod >= 0)
-      (mod, rng2)
-    else nonNegativeLessThan(n)(rng)
-  }
+//   def nonNegativeLessThan(n: Int): Rand[Int] = { rng =>
+//     val (i, rng2) = nonNegativeInt(rng)
+//     val mod = i % n
+//     if (i + (n-1) - mod >= 0)
+//       (mod, rng2)
+//     else nonNegativeLessThan(n)(rng)
+//   }
 
-  // 6.8 :[
-  def flatMap[A, B](s: Rand[A])(f: A => Rand[B]): Rand[B] = rng => {
-    val (a, rng2) = s(rng)
-    f(a)(rng2)
-  }
+//   // 6.8 :[
+//   def flatMap[A, B](s: Rand[A])(f: A => Rand[B]): Rand[B] = rng => {
+//     val (a, rng2) = s(rng)
+//     f(a)(rng2)
+//   }
 
-  // 6.9 :[
-  def _map[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
-  def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = flatMap(ra)(a => _map(rb)(b => f(a, b)))
-}
+//   // 6.9 :[
+//   def _map[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
+//   def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = flatMap(ra)(a => _map(rb)(b => f(a, b)))
+// }
 
 
 
-object ex66 {
+// object ex66 {
 
-  type Rand[A] = State[ex6.RNG, A]
+//   type Rand[A] = State[ex6.RNG, A]
 
-  import State._
+//   import State._
 
-  // 6.10 :[
-  case class State[S, +A](run: S => (A, S)) {
+//   // 6.10 :[
+//   case class State[S, +A](run: S => (A, S)) {
 
-    def flatMap[B](f: A => State[S, B]): State[S, B] =
-      State(s => {
-              val (a, ns) = run(s)
-              f(a).run(ns)
-            })
+//     def flatMap[B](f: A => State[S, B]): State[S, B] =
+//       State(s => {
+//               val (a, ns) = run(s)
+//               f(a).run(ns)
+//             })
 
-    def map[B](f: A => B): State[S, B] =
-     flatMap(a => unit(f(a)))
+//     def map[B](f: A => B): State[S, B] =
+//       flatMap(a => unit(f(a)))
 
-    def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-      flatMap(a => sb.map(b => f(a, b)))
-  }
+//     def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+//       flatMap(a => sb.map(b => f(a, b)))
+//   }
 
-  object State {
-    def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+//   object State {
+//     def unit[S, A](a: A): State[S, A] = State(s => (a, s))
 
-    def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
-      fs.foldRight(unit[S, List[A]](List()))((hr, tr) => hr.map2(tr)(_::_))
-    //                 ^^^^^^^^^^^^ how we were supposed to know to put this here,
-    //                              i have no idea
+//     def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
+//       fs.foldRight(unit[S, List[A]](List()))((hr, tr) => hr.map2(tr)(_::_))
+//     //                 ^^^^^^^^^^^^ how we were supposed to know to put this here,
+//     //                              i have no idea
 
-    def modify[S](f: S => S): State[S, Unit] = for {
-      s <- get // Gets the current state and assigns it to `s`.
-      _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
-    } yield ()
+//     def modify[S](f: S => S): State[S, Unit] = for {
+//       s <- get // Gets the current state and assigns it to `s`.
+//       _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+//     } yield ()
 
-    def get[S]: State[S, S] = State(s => (s, s))
+//     def get[S]: State[S, S] = State(s => (s, s))
 
-    def set[S](s: S): State[S, Unit] = State(_ => ((), s))
-  }
+//     def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+//   }
 
-  // 6.11
-  sealed trait Input
-  case object Coin extends Input
-  case object Turn extends Input
-  case class Machine(locked: Boolean, candies: Int, coins: Int)
+//   // 6.11
+//   sealed trait Input
+//   case object Coin extends Input
+//   case object Turn extends Input
+//   case class Machine(locked: Boolean, candies: Int, coins: Int)
 
-  //            I see what you did there ---VVVVVVVVVVVVV
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
-    State.sequence(
-      inputs.map(input => modify[Machine](machine => machine)))
+//   //            I see what you did there ---VVVVVVVVVVVVV
+//   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+//     State.sequence(
+//       inputs.map(input => modify[Machine](machine => machine)))
 
-  }
-}
+//   }
+// }
