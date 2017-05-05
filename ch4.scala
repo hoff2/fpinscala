d96fee9cd58bd67ef959139cd2e232b1f0c74d51
Charles Hoffman <chuck.hoffman@banno.com>
2017-05-05 11:36:36 -0500

    muck about just so this will work with :load

--- ch4.scala
+++ ch4.scala
@@ -1,36 +1,45 @@
-import scala.{Option => _, Some => _, Either => _, _}
+//package fpinscala.errorhandling
 
-case class Some[+A](get: A) extends Option[A]
-case object None extends Option[Nothing]
+import scala.{Option => _, Either => _, _}
 
-sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = this match {
-    case None => None
-    case Some(a) => Some(f(a))
+object Ch4 {
+
+  sealed trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(a) => Some(f(a))
+    }
+
+    def flatMap[B](f: A => Option[B]): Option[B] =
+      this.map(f).getOrElse(None)
+
+    def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(b) => b
+    }
+
+    def orElse[B >: A](ob: => Option[B]): Option[B] =
+      this.map(Some(_)).getOrElse(ob)
+
+    def filter(f: A => Boolean): Option[A] =
+      this.flatMap(a => if (f(a)) Some(a) else None)
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] =
-    this.map(f).getOrElse(None)
+  case class Some[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
 
-  def getOrElse[B >: A](default: => B): B = this match {
-    case None => default
-    case Some(b) => b
-  }
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] =
-    this.map(Some(_)).getOrElse(ob)
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def filter(f: A => Boolean): Option[A] =
-    this.flatMap(a => if (f(a)) Some(a) else None)
+  def map2[A, B, C](oa: Option[A], ob: Option[B])(f: (A, B) => C): Option[C] =
+    oa.flatMap(a => ob.map(b => f(a, b)))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+    // i gave up on this one after a good fight 
+
+
 }
-
-def mean(xs: Seq[Double]): Option[Double] =
-  if (xs.isEmpty) None
-  else Some(xs.sum / xs.length)
-
-def variance(xs: Seq[Double]): Option[Double] =
-  mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
-
-def map2[A, B, C](oa: Option[A], ob: Option[B])(f: (A, B) => C): Option[C] =
-  oa.flatMap(a => ob.map(b => f(a, b)))
-
