d23766b01e48316bf4869199e55f4ec509a070ce
Charles Hoffman <chuck.hoffman@banno.com>
2017-05-23 14:55:59 -0500

    commenting out to get out of the way of the compiler
    but will probably be deleting altogether once done w ch4

--- ex4.scala
+++ ex4.scala
@@ -1,140 +1,79 @@
-import scala.{Option => _, Either => _, _}
+// object Ch4 {
 
-object Ex4 {
+//   def sequenceC[A](a: List[Option[A]]): Option[List[A]] = a match {
+//     case Nil => Some(Nil)
+//       case (None :: t) => None
+//       case (Some(v) :: t) => sequence(t).map{ tt => v :: tt }
+//     }
 
-  //=== OPTION ===
+//     def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+//       case Nil => Some(Nil)
+//       case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+//     }
 
-  case class Some[+A](get: A) extends Option[A]
-  case object None extends Option[Nothing]
+//     // 4.5 (wrong)
+//     // def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+//     //   case Nil => Some(Nil)
+//     //   case h :: t => f(h) flatMap(hh => sequence(t)(f) map (hh :: _))
+//     // }
+//     def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+//       a.foldRight[Option[List[B]]](Some(Nil))((h,t) => map2(f(h),t)(_ :: _))
 
-  // 4.1
-  sealed trait Option[+A] {
-    def map[B](f: A => B): Option[B] = this match {
-      case None => None
-      case Some(a) => Some(f(a))
-    }
+//     def sequenceWithTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity _)
+//   }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = this match{
-      case None    => None
-      case Some(a) => f(a)
-    }
+//   //=== EITHER ===
 
-    def getOrElse[B >: A](default: => B): B = this match {
-      case None    => default
-      case Some(a) => a
-    }
+//   sealed trait Either[+E, +A] {
+//     def map[B](f: A => B): Either[E, B] = this match {
+//       case Left(e) => Left(e)
+//       case Right(a) => Right(f(a))
+//     }
 
-    def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
-      case None    => ob
-      case Some(a) => Some(a) // thought about "this", wasn't sure
-    }
+//     def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+//       case Left(e) => Left(e)
+//       case Right(a) => f(a)
+//     }
 
-    def filter(f: A => Boolean): Option[A] = this match {
-      case Some(a) if f(a) => Some(a) //likewise
-      case _ => None
-    }
-  }
+//     def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+//       case Left(_) => b
+//       case Right(a) => Right(a)
+//     }
 
-  object Option {
-    // 4.2
-    def mean(xs: Seq[Double]): Option[Double] =
-      if (xs.nonEmpty) Some(xs.sum / xs.size)
-      else None
+//     def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C):
+//         Either[EE, C] = for {
+//       aa <- this
+//       bb <- b
+//     } yield(f(aa, bb))
+//   }
 
-    def variance(xs: Seq[Double]): Option[Double] =
-      mean(xs).flatMap(m => mean(xs.map(x => math.pow(x-m, 2))))
+//   case class Left[+E](value: E) extends Either[E, Nothing]
+//   case class Right[+A](value: A) extends Either[Nothing, A]
 
-    // 4.3
-    // def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
-    //   case (Some(a), Some(b)) => Some(f(a, b))
-    //   case _ => None
-    // }
+//   object Either {
+//     // def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
+//     //   case Nil => Right(Nil)
+//     //   case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+//     // }
 
-    // def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-    //   for {
-    //     aa <- a
-    //     bb <- b
-    //   } yield f(aa, bb)
+//     // def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+//     //   case Nil => Right(Nil)
+//     //   case h :: t => h flatMap (hh => traverse(t)(f) map (hh :: _))
+//     // }
+//   }
 
-    def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-      a flatMap (aa => b map (bb => f(aa, bb)))
+//   case class Person(name: Name, age: Age)
+//   sealed class Name(val value: String)
+//   sealed class Age(val value: Int)
 
-    // 4.4
+//   def mkName(name: String): Either[String, Name] =
+//     if (name == "" || name == null) Left("Name is empty.")
+//     else Right(new Name(name))
 
-    def sequenceC[A](a: List[Option[A]]): Option[List[A]] = a match {
-      case Nil => Some(Nil)
-      case (None :: t) => None
-      case (Some(v) :: t) => sequence(t).map{ tt => v :: tt }
-    }
+//   def mkAge(age: Int): Either[String, Age] =
+//     if (age < 0) Left("Age is out of range.")
+//     else Right(new Age(age))
 
-    def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
-      case Nil => Some(Nil)
-      case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
-    }
-
-    // 4.5 (wrong)
-    // def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
-    //   case Nil => Some(Nil)
-    //   case h :: t => f(h) flatMap(hh => sequence(t)(f) map (hh :: _))
-    // }
-    def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
-      a.foldRight[Option[List[B]]](Some(Nil))((h,t) => map2(f(h),t)(_ :: _))
-
-    def sequenceWithTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity _)
-  }
-
-  //=== EITHER ===
-
-  sealed trait Either[+E, +A] {
-    def map[B](f: A => B): Either[E, B] = this match {
-      case Left(e) => Left(e)
-      case Right(a) => Right(f(a))
-    }
-
-    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
-      case Left(e) => Left(e)
-      case Right(a) => f(a)
-    }
-
-    def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
-      case Left(_) => b
-      case Right(a) => Right(a)
-    }
-
-    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C):
-        Either[EE, C] = for {
-      aa <- this
-      bb <- b
-    } yield(f(aa, bb))
-  }
-
-  case class Left[+E](value: E) extends Either[E, Nothing]
-  case class Right[+A](value: A) extends Either[Nothing, A]
-
-  object Either {
-    // def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
-    //   case Nil => Right(Nil)
-    //   case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
-    // }
-
-    // def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
-    //   case Nil => Right(Nil)
-    //   case h :: t => h flatMap (hh => traverse(t)(f) map (hh :: _))
-    // }
-  }
-
-  case class Person(name: Name, age: Age)
-  sealed class Name(val value: String)
-  sealed class Age(val value: Int)
-
-  def mkName(name: String): Either[String, Name] =
-    if (name == "" || name == null) Left("Name is empty.")
-    else Right(new Name(name))
-
-  def mkAge(age: Int): Either[String, Age] =
-    if (age < 0) Left("Age is out of range.")
-    else Right(new Age(age))
-
-  def mkPerson(name: String, age: Int): Either[String, Person] =
-    mkName(name).map2(mkAge(age))(Person(_, _))
-}
+//   def mkPerson(name: String, age: Int): Either[String, Person] =
+//     mkName(name).map2(mkAge(age))(Person(_, _))
+// }
