b04674f38ccb8f519dc24f82f3ffd6e1025aff36
Charles Hoffman <chuck.hoffman@banno.com>
2017-06-19 17:43:56 -0500

    to 6.2

--- src/main/scala/ch6.scala
+++ src/main/scala/ch6.scala
@@ -1,4 +1,33 @@
-object Ch6{
+import scala.util.Random
 
+object Ch6 {
 
+  trait RNG { def nextInt: (Int, RNG) }
+
+  case class SimpleRNG(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  // 6.1
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, g) = rng.nextInt
+    (if (i == Int.MinValue) 0 else scala.math.abs(i), g)
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, g) = rng.nextInt
+    (i.toDouble / Int.MaxValue, g)
+  }
+
+  def main(args: Array[String]):Unit = {
+    println("========================================")
+    val rng = SimpleRNG(239847923)
+    println(nonNegativeInt(rng))
+    println(double(rng))
+  }
 }
