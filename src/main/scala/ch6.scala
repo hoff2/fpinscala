e1c403270978b211aa8c8c96a31e09414a43d9b7
Charles Hoffman <chuck.hoffman@banno.com>
2017-06-19 18:01:21 -0500

    little more of ch 6

--- src/main/scala/ch6.scala
+++ src/main/scala/ch6.scala
@@ -13,15 +13,42 @@ object Ch6 {
     }
   }
 
-  // 6.1
+  // 6.1 - 6.4
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, g) = rng.nextInt
-    (if (i == Int.MinValue) 0 else scala.math.abs(i), g)
+    ( if (i == Int.MinValue) 0
+      else scala.math.abs(i),
+      g )
   }
 
   def double(rng: RNG): (Double, RNG) = {
-    val (i, g) = rng.nextInt
+    val (i, g) = nonNegativeInt(rng)
     (i.toDouble / Int.MaxValue, g)
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, g1) = nonNegativeInt(rng)
+    val (d, g2) = double(g1)
+    ((i, d), g2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, g1) = double(rng)
+    val (i, g2) = nonNegativeInt(g1)
+    ((d, i), g2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, g1) = double(rng)
+    val (d2, g2) = double(g1)
+    val (d3, g3) = double(g2)
+    ((d1, d2, d3), g3)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    val (i, g) = nonNegativeInt(rng)
+    if (count == 0) (List(), g)
+    else (i :: ints(count - 1)(g)._1, g)
   }
 
   def main(args: Array[String]):Unit = {
@@ -29,5 +56,6 @@ object Ch6 {
     val rng = SimpleRNG(239847923)
     println(nonNegativeInt(rng))
     println(double(rng))
+    println(ints(5)(rng))
   }
 }
