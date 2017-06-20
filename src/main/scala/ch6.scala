c648b216d15e46a24f275606bd584619e4978824
Charles Hoffman <chuck.hoffman@banno.com>
2017-06-20 18:03:02 -0500

    a little more

--- src/main/scala/ch6.scala
+++ src/main/scala/ch6.scala
@@ -51,11 +51,33 @@ object Ch6 {
     else (i :: ints(count - 1)(g)._1, g)
   }
 
-  def main(args: Array[String]):Unit = {
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a:A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  // 6.5
+  def mdouble: Rand[Double] = {
+    map(nonNegativeInt)(_.toDouble / Int.MaxValue)
+  }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a, b), rng3)
+    }
+
+  def main(args: Array[String]): Unit = {
     println("========================================")
     val rng = SimpleRNG(239847923)
-    println(nonNegativeInt(rng))
-    println(double(rng))
-    println(ints(5)(rng))
+    println(mdouble(rng))
   }
 }
